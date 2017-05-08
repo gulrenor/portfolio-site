420bbbf83cf1c8a3b397a4f39a91cb0bb776e0e1
Tim <gulrenor@gmail.com>
2017-05-08 12:22:40 -0400

    merge

--- Gulpfile.js
+++ Gulpfile.js
@@ -15,6 +15,9 @@ var cssnano = require('gulp-cssnano');
 var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
 
+// Pug
+var pug = require('gulp-pug');
+
 // Hugo
 var exec = require('child_process').exec;
 var bg = require('gulp-bg');
@@ -22,9 +25,11 @@ var bg = require('gulp-bg');
 // Hugo config
 var hugoTheme = 'kal';
 var dirSass_In = 'src/sass/**/*.scss';
-var dirCSS_Out = 'css/';
+var dirCSS_Out = 'www/css/';
 var dirJS_In = 'src/js/**/*.js';
-var dirJS_Out = 'js/';
+var dirJS_Out = 'www/js/';
+var dirPug_In = 'src/pug/**/!(_)*.pug'; // Prevents _includes
+var dirHTML_Out = 'www/';
 
 // Hugo
 gulp.task('hugo-build', function(callback) {
@@ -65,6 +70,14 @@ gulp.task('scripts', function() {
     .pipe(gulp.dest(dirJS_Out));
 });
 
+// Pug task
+gulp.task('html', function() {
+  gulp.src(dirPug_In)
+    .pipe(plumber())
+    .pipe(pug({ pretty: true }))
+    .pipe(gulp.dest(dirHTML_Out));
+})
+
 //TODO: Add SVG minify task
 
 // Watch
@@ -73,4 +86,5 @@ gulp.task('default', function() {
   //gulp.watch(['hugo-server']);
   gulp.watch(dirSass_In, ['styles']);
   gulp.watch(dirJS_In, ['scripts']);
+  gulp.watch(dirPug_In, ['html']);
 });
