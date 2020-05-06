dd69f045c5a9ab91af56d059aff7063e2760b426
Tim <timothyw.harrison@gmail.com>
2020-05-06 17:07:40 -0400

    wip

--- src/js/contact.js
+++ src/js/contact.js
@@ -5,46 +5,41 @@ $(function() {
   $(form).submit(function(event) {
     event.preventDefault(); //stop html from submitting form
 
-    // Check recaptcha was checked
     try {
-      if (grecaptcha.getResponse() == '') {
-        throw new Error('Please prove you\'re not a bot.');
-      } else {
-        var formData = $(form).serialize();
+      var formData = $(form).serialize();
 
-        // submit form
-        $.ajax({
-            type: 'POST',
-            url: $(form).attr('action'), //uses the default action from html
-            data: formData
-          })
-          // display success
-          .done(function(response) {
-            $(formMessages).removeClass('error');
-            $(formMessages).addClass('success');
-            $('#contact-name').prop('readonly', 'readonly');
-            $('#contact-email').prop('readonly', 'readonly');
-            $('#contact-comment').prop('readonly', 'readonly');
-            $('#contact-submit').prop('disabled', 'disabled');
-            $('#contact-submit').html(response);
-          })
+      // submit form
+      $.ajax({
+          type: 'POST',
+          url: $(form).attr('action'), //uses the default action from html
+          data: formData
+        })
+        // display success
+        .done(function(response) {
+          $(formMessages).removeClass('error');
+          $(formMessages).addClass('success');
+          $('#contact-name').prop('readonly', 'readonly');
+          $('#contact-email').prop('readonly', 'readonly');
+          $('#contact-comment').prop('readonly', 'readonly');
+          $('#contact-submit').prop('disabled', 'disabled');
+          $('#contact-submit').html(response);
+        })
 
-          //display error
-          .fail(function(data) {
-            $(formMessages).removeClass('success');
-            $(formMessages).addClass('error');
-            if (data.responseText !== '') {
-              $(formMessages).text(data.responseText);
-            } else {
-              $(formMessages).text('An error occured.');
-            }
-          });
-      };
+        //display error
+        .fail(function(data) {
+          $(formMessages).removeClass('success');
+          $(formMessages).addClass('error');
+          if (data.responseText !== '') {
+            $(formMessages).text(data.responseText);
+          } else {
+            $(formMessages).text('An error occured.');
+          }
+        });
     }
     catch(err) {
       $(formMessages).removeClass('success');
       $(formMessages).addClass('error');
-      $(formMessages).text('Please prove you\'re not a robot.');
+      $(formMessages).text('Something went wrong.');
     }
   });
 });
--- www/mail/mail.php
+++ www/mail/mail.php
@@ -1,34 +1,70 @@
 <?php
-  $ip = getUserIP();
-  
-  function recaptcha() {
-    include 'recaptcha_auth.php';
-    
-    $url = "https://www.google.com/recaptcha/api/siteverify";
-    $response = $_POST["g-recaptcha-response"];
 
-    $data = array(
-      'secret' => $secret,
-      'response' => $response
-    );
-    
-    $options = array(
-      'http' => array (
-        'method' => 'POST',
-        'content' => http_build_query($data)
-      )
-    );
-    
-    $context = stream_context_create($options);
-    $verify = file_get_contents($url, false, $context);
-    
-    $recaptcha = json_decode($verify);
-    if ($recaptcha->success==true) {
-      return true;
-    } else if ($recaptcha->success==false) {
-      return false;
+  // Test
+  // echo "IP=" . $ip;
+  // // print_r($_POST);
+  // if (checkHoneypot()) {
+  //   echo "it returned true";
+  // } else {
+  //   echo "it returned false?";
+  // }
+
+  function recaptchav3() {
+    // Make sure the form has been submitted
+    if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['recaptcha_response'])) {
+
+      // Get the site key for verification
+      include 'recaptcha_auth.php';
+
+      // Build the verification request for Google
+      $recaptcha_url = 'https://www.google.com/recaptcha/api/siteverify';
+      $recaptcha_response = $_POST['recaptcha_response'];
+
+      // Interpret the response
+      $recaptcha = file_get_contents($recaptcha_url . '?secret=' . $recaptcha_secret . '&response=' . $recaptcha_response);
+      $recaptchatwo = json_decode($recaptcha);
+
+      // You can edit the score here to adjust how strict it is
+      if ($recaptchatwo->score >= 0.5) {
+        // Ok to send the email
+        echo "ok to send";
+        return true
+      } else {
+        // Return an error, or maybe just silence?
+        echo "likely spammer";
+        return false
+      }
     }
   }
+  
+  // // function recaptcha() {
+  // //   include 'recaptcha_auth.php';
+    
+  // //   $url = "https://www.google.com/recaptcha/api/siteverify";
+  // //   $response = $_POST["g-recaptcha-response"];
+
+  // //   $data = array(
+  // //     'secret' => $secret,
+  // //     'response' => $response
+  // //   );
+    
+  // //   $options = array(
+  // //     'http' => array (
+  // //       'method' => 'POST',
+  // //       'content' => http_build_query($data)
+  // //     )
+  // //   );
+    
+  // //   $context = stream_context_create($options);
+  // //   $verify = file_get_contents($url, false, $context);
+    
+  // //   $recaptcha = json_decode($verify);
+  // //   if ($recaptcha->success==true) {
+  // //     return true;
+  // //   } else if ($recaptcha->success==false) {
+  // //     return false;
+  // //   }
+  // // }
   
   function getUserIP(){
   // For proxy users
@@ -51,13 +87,33 @@
     return $ip;
   }
 
+  function checkHoneypot(){
+    // If any of the honeypot fields are filled in, then we know it's a bot
+    if ($_POST['name'] !== "") {
+      return false;
+    }
+    if ($_POST['email'] !== "") {
+      return false;
+    }
+    if ($_POST['comment'] !== "") {
+      return false;
+    }
+    return true;
+  }
+
+  $honeypot = "honeypot usage detected";
+  if (checkHoneypot()) {
+    $honeypot = "no honeypot usage";
+  }
+
   $from = 'twharrison.com';
   $subject = 'Website Comment';
   include 'auth.php';
+  $ip = getUserIP();
 
-  $contactname = $_POST['contact-name'];
-  $contactemail = $_POST['contact-email'];
-  $contactcomment = $_POST['contact-comment'];
+  $contactname = $_POST['contact-name6LcK'];
+  $contactemail = $_POST['contact-email6LcK'];
+  $contactcomment = $_POST['contact-comment6LcK'];
   
   $headers = "MIME-Version: 1.0" . "\r\n";
   $headers .= "Content-type: text/html; charset=iso-8859-1" . "\r\n";
@@ -70,15 +126,15 @@
   $content .= "<tr><td><span>From</span></td><td><span>" . $contactname . "</span></td></tr>";
   $content .= "<tr><td><span>Email</span></td><td><span>" . $contactemail . "</span></td></tr>";
   $content .= "<tr><td><span>Browser IP</span></td><td><span>" . $ip . "</span></td></tr>";
-  $content .= "<tr><td colspan=\"2\"><span>" . $contactcomment . "</span>
-  </td></tr></table>";
-  
-  // Send the email
-  if (recaptcha()) {
-    mail($to,$subject,$content,$headers) or die('There was an error sending mail.');
-    echo 'Thanks, your message has been sent!';
-  } else {
-    echo 'There was an error, please try again';
-  }
+  $content .= "<tr><td colspan=\"2\"><span>" . $contactcomment . "</span></td></tr>";
+  $content .= "<tr><td><span>Honeypot Status</span></td><td><span>" . $honeypot . "</span></td></tr></table>";
+
+  // // Send the email
+  // if (recaptchav3()) {
+  //   mail($to,$subject,$content,$headers) or die('There was an error sending mail.');
+  //   echo 'Thanks, your message has been sent!';
+  // } else {
+  //   echo 'There was an error, please try again';
+  // }
 
 ?>
