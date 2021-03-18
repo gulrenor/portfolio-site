773ab9470b7460f49620231e9654001145aa85d7
Tim <timothyw.harrison@gmail.com>
2021-03-18 16:06:32 -0400

    remove logging info

--- src/js/common.js
+++ src/js/common.js
@@ -1,6 +1,5 @@
 // Current year used for copyright
 $(document).ready(function() {
 	var thisYear = new Date().getFullYear();
-	console.log(thisYear);
 	$('#copyright-year').html(thisYear);	
 });
--- src/js/navmenu.js
+++ src/js/navmenu.js
@@ -9,7 +9,6 @@ $(document).ready(function() {
   };
 
   $(window).resize(function() {
-    console.log(checkBreakpoint());
     if (!checkBreakpoint()) {
       $('nav').show(300);
     }
@@ -17,33 +16,14 @@ $(document).ready(function() {
 
   $('.hamburger').click(function() {
     if (checkBreakpoint()) {
-      console.log('clicked small');
       $('nav').toggle(300);
     }
   });
 
   $('.nav-menu a').click(function() {
     if (checkBreakpoint()) {
-      console.log('clicked nav menu when small');
       $('nav').toggle(300);
     }
   });
 
 });
-
-/*
-$(window).resize(
-    function() {
-        if (window.matchMedia('(max-width: 768px)').matches) {
-            console.log('small');
-            $('.nav-menu a').click(function() {
-                console.log('menu clicked when small');
-                $('nav').toggle(300);
-            });
-        } else {
-            console.log('not small')
-            $('nav').show(300);
-        };
-    }
-);
-*/
